9cdd80e7dd48df697579ed81f3ae10d101f778da
Slava Mokrynskyi <chempionslava@gmail.com>
2024-11-23 16:13:00 +0200

    Add project files.

---
+++ OOP-07/OOP-07.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Overcoat
+{
+    string type;
+    float price;
+public:
+    Overcoat(): type("none"), price(0) {}
+    Overcoat(string type, float price) : type(type), price(price) {}
+    bool operator==(const Overcoat& other)const
+    {
+        return this->type == other.type;
+    }
+    Overcoat& operator=(const Overcoat& other)
+    {
+        this->type = other.type;
+        this->price = other.price;
+        return *this;
+    }
+    bool operator>(const Overcoat& other)
+    {
+        return price > other.price;
+    }
+};
+
+class Flat
+{
+    int square;
+    float price;
+public:
+    Flat() :square(0), price(0) {}
+    Flat(int square, float price) : square(square), price(price) {}
+    bool operator==(const Flat& other)const
+    {
+        return this->square == other.square;
+    }
+    Flat& operator=(const Flat& other)
+    {
+        this->square = other.square;
+        this->price = other.price;
+        return *this;
+    }
+    bool operator>(const Flat& other)
+    {
+        return price > other.price;
+    }
+};
+
+int main()
+{
+    
+}
